a5d60d901896dbad9e9ffbae5a9bc8083d46b3be
Jesús Silva-Rodríguez <jsilva-ibis@us.es>
2023-08-29 17:40:19 +0200

    Update image_manip.py
    Generalized the niitoanalyze function to convert both ways

--- image_manip.py
+++ image_manip.py
@@ -39,27 +39,50 @@ def apply_constant_to_img(image, c, operation, output=False):
     nib.save(new_img, output_file)
 
 
-def convert_to_analyze(nii_file):
-    """This function converts a nifti file to an analyze file and saves it in the same directory.
+def convert_format(img_name, compress_out=True):
+    """This function converts an image file from nifti to analyze and viceversa.
+    Depending on your input, it will output the other format. It also works with .nii.gz
 
-    :param nii_file: the name of the nifti file to be converted
-    :type nii_file: str
-    :return: the name of the analyze file that was created
+    :param img_name: the name of the nii/nii.gz/hdr file to be converted
+    :type img_name: str
+    :param compress_out: Will out nii.gz if the input is analyze
+    :type img_name: bool
+    :return: the name of the result file that was created
     :rtype: str
     """
 
     # Load the NIFTI image using nibabel
-    img = nib.load(nii_file)
-
-    # Get the header information from the NIFTI image
-    header = img.header
-
-    # Get the image data from the NIFTI image
+    img = nib.load(img_name)
     data = img.get_fdata()
 
-    # Save the Analyze image
-    analyze_img = nib.analyze.AnalyzeImage(data, header.get_best_affine(), header)
-    analyze_file = nii_file.split('.nii.gz')[0] + '.img'
-    nib.save(analyze_img, analyze_file)
-    print(f"File {nii_file} was converted to Analyze format and saved as {analyze_file}")
-    return analyze_file
+    if img[-4:] == ".hdr" or img[-4:] == '.img':
+
+        out_img = nib.Nifti1Image(data, img.affine, img.header)
+
+        if compress_out:
+            out_name = img[-4:] + '.nii.gz'
+        else:
+            out_name = img[-4:] + '.nii'
+
+        nib.save(out_img,out_name)
+        print(f"File {img_name} was converted to Nifti format and saved as {out_name}")
+
+        return out_name
+
+    elif img[-4:] == ".nii" or img[-7:] == '.nii.gz':
+    
+        analyze_img = nib.analyze.AnalyzeImage(data, img.affine, img.header)
+
+        if img[-4:] == ".nii":
+            out_name = img[-4:] + '.hdr'
+        else:
+            out_name = img[-7:] + '.hdr'
+
+        nib.save(analyze_img, out_name)
+        print(f"File {img_name} was converted to Analyze format and saved as {out_name}")
+
+        return out_name
+    
+    else:
+
+        raise TypeError("Wrong format")
