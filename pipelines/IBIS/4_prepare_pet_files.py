dfd73aad07f546126284ab231c360e27c7ebfbe4
Jesús Silva-Rodríguez <jsilva-ibis@us.es>
2024-04-09 11:39:30 +0200

    Update 4_prepare_pet_files.py

--- pipelines/IBIS/4_prepare_pet_files.py
+++ pipelines/IBIS/4_prepare_pet_files.py
@@ -1,7 +1,7 @@
 import os
-from os.path import join
+from os.path import join, exists
 
-dir_patients = '/mnt/c/Users/jesus/Downloads/Invivo_2024/OUT'
+dir_patients = '/mnt/d/IBIS_DATA/Reorder_New'
 
 list_dirs = os.listdir(dir_patients)
 
@@ -9,26 +9,20 @@ for i in list_dirs:
 
     dir_subj = join(dir_patients, i)
 
-    pet_image = False
-
     files_ = os.listdir(dir_subj)
 
     for file_ in files_:
 
-        # if file_[0:2] == 'IN':
-        if file_[-3:] == 'nii':
-            # if 'FDG' in file_:
-            pet_image = join(dir_subj, file_)
+        if file_[0:2] == 'IN' and file_[-3:] == 'nii' and 'FDG' in file_:
 
-    if pet_image:
-        # pet_dir = join(dir_subj, 'fdg')
+            pet_in = join(dir_subj, file_)
+            date = file_[-12:-4]
 
-        # if not exists(pet_dir):
-        #    os.makedirs(pet_dir)
+            pet_dir = join(dir_subj, 'fdg_%s' % date)
 
-        # if exists(pet_dir):
-        #    shutil.rmtree(pet_dir)
-        # os.makedirs(pet_dir)
+            if not exists(pet_dir):
+                os.makedirs(pet_dir)
 
-        pet_in = join(dir_subj, 'pet.img')
-        os.system('niitoanalyze %s %s' % (pet_image, pet_in))
+            pet_ = join(pet_dir, 'pet.img')
+            os.system('niitoanalyze %s %s' % (pet_in, pet_))
+            print("Created %s" % pet_)
